7410ade675692da2dbec51c90fd73a024b07a1a4
dssasmaciel <ds.sasmaciel@gmail.com>
2025-09-16 04:33:27 -0300

    Desenvolvimento do APP e Implementação de Alertas

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,25 +1,62 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Net;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                        if (t != null)
+                        {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Pôr do Sol: {t.sunset} \n" +
+                                         $"Temperatura Mínima: {t.temp_min} \n" +
+                                         $"Temperatura Máxima: {t.temp_max} \n" +
+                                         $"Condição: {t.main} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do Vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n";                                       ;
+
+                         lbs_res.Text = dados_previsao;
+
+                        }
+                }
+                else
+                {
+                    lbs_res.Text = "Por favor, digite o nome da cidade.";
+                }
+            }
+            catch (InvalidOperationException ex) 
+            {
+                await DisplayAlert("Erro de Busca", "A cidade informada não foi encontrada. Verifique o nome e tente novamente.", "Ok");
+                lbs_res.Text = "Cidade não encontrada.";
+            }
+            catch (HttpRequestException ex) 
+            {
+                await DisplayAlert("Erro de Conexão ❌", "Não foi possível conectar ao serviço de clima. Verifique sua conexão com a internet.", "Ok");
+                lbs_res.Text = "Falha na conexão.";
+            }
+            catch (Exception ex) 
+            {
+                await DisplayAlert("Erro Inesperado ⚠️", $"Ocorreu um erro: {ex.Message}", "Ok");
+                lbs_res.Text = "Erro inesperado.";
+            }
         }
     }
-
 }
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,66 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "062974aa3b27059b0718e805a2c61e64";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+            try
+            {
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+                        var rascunho = JObject.Parse(json);
+
+                        DateTime time = new();
+                        DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                        DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                        t = new()
+                        {
+                            lon = (double)rascunho["coord"]["lon"],
+                            lat = (double)rascunho["coord"]["lat"],
+                            temp_min = (double)rascunho["main"]["temp_min"],
+                            temp_max = (double)rascunho["main"]["temp_max"],
+                            visibility = (int)rascunho["visibility"],
+                            speed = (double)rascunho["wind"]["speed"],
+                            main = (string)rascunho["weather"][0]["main"],
+                            description = (string)rascunho["weather"][0]["description"],
+                            sunrise = sunrise.ToString(),
+                            sunset = sunset.ToString(),
+                        };
+                    }
+                    else if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        throw new InvalidOperationException("Cidade não encontrada no serviço de clima. ");
+                    }
+                    else
+                    {
+                        throw new Exception($"Erro na API: {resp.StatusCode}");
+                    }
+                 } 
+            }
+                catch (HttpRequestException ex)
+            {
+                   throw;
+            }
+            return t;
+        }
+
+    }
+}
+       
